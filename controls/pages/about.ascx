7df4a31c894637bcd89af12ecf3714d7166ab14b
Jeremy Burton <jermbo002@gmail.com>
2015-11-23 03:12:31 -0600

    Service worker.

--- controls/pages/about.ascx
+++ controls/pages/about.ascx
@@ -3,7 +3,7 @@
 	<div class="content content-container-xsm">
 		<h2 class="copy-hdr copy-hdr-xl text-center">Passionate Web Developers Striving to Build a Better Web.</h2>
 		<p>At <strong class="color-primary">Select Interactive</strong> we create websites, develop web and mobile applications, and build custom content management systems for businesses, large or small.</p>
-		<p>We love a good challenge and build our business on being industry experts on up-and-coming web trends, platforms, and applications. If you can imagine it, there’s a good chance we can build it. Let us bring digital voice to your brand.</p>
+		<p>We love a good challenge and build our business on being industry experts on up-and-coming web trends, platforms, and applications. If you can imagine it, there&rsquo;s a good chance we can build it. Let us bring digital voice to your brand.</p>
 		<p>We look forward to working with you.</p>
 	</div>
 </div>
--- serviceworker.js
+++ serviceworker.js
@@ -7,13 +7,15 @@
 importScripts( '/js/libs/serviceworker-cache-polyfill.js' );
 
 // cache version
-var CACHE_VERSION = 'cache-v-1';
+var CACHE_VERSION = 'cache-v-2';
 
 // Files we want to cache
 var urlsToCache = [
 	'/bower_components/es6-promise/promise.min.js',
 	'/bower_components/fetch/fetch.js',
-	'/js/libs/picturefill.min.js'
+	'/js/libs/picturefill.min.js',
+	'/css/styles.v-3.0.min.css',
+	'/js/app/build/main.v-3.2.min.js'
 ];
 
 // set the callback for the install step
@@ -29,31 +31,50 @@ self.addEventListener( 'fetch', function( event ) {
 	var requestUrl = new URL( event.request.url );
 
 	event.respondWith(
+		// check if the item is in the cache
+		//   and if it is load from the cache
 		caches.match( event.request ).then( function( response ) {
-			// cache hit -- return response
 			if ( response ) {
 				return response;
 			}
 
+			// if the item is not in the cache,
+			//   we need to clone the request
 			var fetchRequest = event.request.clone();
 
+			// attempt to fetch the requested item from the network
 			return fetch( fetchRequest ).then( function( response ) {
 				if ( !response || response.status !== 200 || response.type !== 'basic' ) {
 					return response;
 				}
 
-				var responseToCache = response.clone();
+				var rspClone = response.clone();
 
+				// let's check to see if the requested item is an image
+				//   and if it is then cache it
 				if ( requestUrl.pathname.indexOf( '/img/' ) === 0 ) {
-					console.log( 'Should cache: ' + requestUrl.pathname );
-					//caches.open( CACHE_VERSION ).then( function( cache ) {
-					//	cache.put( event.request, responseToCache ).then( function() {
-					//		console.log( 'Image added to the cache.' );
-					//	} );
-					//} );
+					caches.open( CACHE_VERSION ).then( function( cache ) {
+						cache.put( fetchRequest, response ).then( function() {
+							console.log( 'Image added to the cache' );
+						}, function() {
+							console.log( 'Image not added to the cache' );
+						} );
+					} );
+				}
+				else if ( requestUrl.pathname.indexOf( '/font/' ) === 0 ) {
+					caches.open( CACHE_VERSION ).then( function( cache ) {
+						cache.put( fetchRequest, response ).then( function() {
+							console.log( 'Font added to the cache' );
+						}, function() {
+							console.log( 'Font not added to the cache' );
+						} );
+					} );
 				}
 
-				return response;
+				return rspClone;
+			} ).catch( function() {
+				// the requested item wasn't in the cache and was unable to be downloaded from
+				//   the network -- likely due to offline
 			} );
 		} )
 	);
